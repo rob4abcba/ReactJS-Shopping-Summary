a09e2f6349cf4bf940ddc79a3cb2fb8cada1df6e
rob4abcba <rob_abcba@yahoo.com>
2019-07-16 09:48:45 -0700

    Add Subtotal component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import React from "react";
 // import Container from 'react-bootstrap/Container'
 import { Container, Row, Col } from "react-bootstrap";
 import "./App.css";
+import Subtotal from './components/Subtotal/Subtotal';
 
-function App() {
-  return (
+class App extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        total: 100
+      };
+    }
+
+    render() {
+    return (
     <div className="container">
       <Container className="purchase-card">
-        <h1>Hello Federal</h1>
+        <Subtotal price={this.state.total.toFixed(2)} />
       </Container>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
